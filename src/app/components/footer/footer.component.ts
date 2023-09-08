12f5d6d63f802c215e0253f6d20c02e6a4a78524
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-09-08 20:20:25 +0400

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  isHovered = false;
 
+  onMouseEnter() {
+    this.isHovered = true;
+  }
+
+  onMouseLeave() {
+    this.isHovered = false;
+  }
 }
