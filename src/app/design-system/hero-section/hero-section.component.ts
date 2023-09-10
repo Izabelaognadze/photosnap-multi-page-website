2aa1db1d09ec34a0b5f5eeee74244e2d17e19b89
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-09-10 23:23:46 +0400

    hero section

--- src/app/design-system/hero-section/hero-section.component.ts
+++ src/app/design-system/hero-section/hero-section.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
   styleUrls: ['./hero-section.component.css']
 })
-export class HeroSectionComponent {
+export class HeroSectionComponent implements OnInit {
+  @Input() public mobImg = "";
+  @Input() public tabImg = "";
+  @Input() public deskImg = "";
 
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 }
